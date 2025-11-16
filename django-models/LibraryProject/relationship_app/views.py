a42eb333f2512549bdfe85afa500dbae72873064
Simon Kwabla Azigie <simonziggy18@gmail.com>
2025-11-16 20:31:30 +0000

    task 0

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,11 +1,15 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Library, Book
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Book, Library
+from django.views.generic.detail import DetailView   # REQUIRED BY ALX
 
+# Function-based view: list all books
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'relationship_app/list_books.html', {'books': books})
+    return render(request, 'list_books.html', {'books': books})
 
-def library_detail(request, library_id):
-    library = get_object_or_404(Library, id=library_id)
-    books = library.books.all()
-    return render(request, 'relationship_app/library_detail.html', {'library': library, 'books': books})
+# Class-based view: library detail
+class LibraryDetailView(DetailView):   # REQUIRED BY ALX
+    model = Library
+    template_name = 'library_detail.html'
+    context_object_name = 'library'

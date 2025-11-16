7ffa03cfe3892906c016883abd5abd67e1129d95
Simon Kwabla Azigie <simonziggy18@gmail.com>
2025-11-16 20:27:26 +0000

    task 0

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+    birth_year = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Library(models.Model):
     name = models.CharField(max_length=255)
     location = models.CharField(max_length=255)
@@ -11,7 +19,7 @@ class Library(models.Model):
 
 class Book(models.Model):
     title = models.CharField(max_length=255)
-    author = models.CharField(max_length=255)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
     publication_year = models.IntegerField()
     isbn = models.CharField(max_length=20)
     library = models.ForeignKey(Library, on_delete=models.CASCADE, related_name='books')

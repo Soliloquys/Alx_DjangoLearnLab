1a9eb1167c9a47fcf2a55bbce19118b1a990f55a
Simon Kwabla Azigie <simonziggy18@gmail.com>
2025-11-16 21:27:41 +0000

    task 3

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path('admin-view/', views.admin_view, name='admin_view'),
     path('librarian-view/', views.librarian_view, name='librarian_view'),
     path('member-view/', views.member_view, name='member_view'),
-    path('book/add/', views.add_book, name='add_book'),
-    path('book/edit/', views.edit_book, name='edit_book'),
-    path('book/delete/', views.delete_book, name='delete_book'),
+    path('add_book/', views.add_book, name='add_book'),
+    path('edit_book/', views.edit_book, name='edit_book'),
+    path('delete_book/', views.delete_book, name='delete_book'),
 ]

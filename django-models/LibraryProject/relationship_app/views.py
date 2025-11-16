d7d167026350f282a90c45451d10e5b980f39ef6
Simon Kwabla Azigie <simonziggy18@gmail.com>
2025-11-16 21:13:54 +0000

    task 3

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -46,3 +46,25 @@ def logout_view(request):
     logout(request)
     return render(request, 'relationship_app/logout.html')
 
+from django.contrib.auth.decorators import user_passes_test
+
+def admin_check(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+def librarian_check(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+
+def member_check(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+
+@user_passes_test(admin_check)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@user_passes_test(librarian_check)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@user_passes_test(member_check)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')

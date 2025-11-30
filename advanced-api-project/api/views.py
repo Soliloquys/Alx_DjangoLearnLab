28ef15d5743a99d2591ffdc63d4c896994fbbe1e
Simon Kwabla Azigie <simonziggy18@gmail.com>
2025-11-30 23:32:04 +0000

    advanced api

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,7 +1,8 @@
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+
 
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()

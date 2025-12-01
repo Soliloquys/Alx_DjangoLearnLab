74849bf2f0ada80ab2c6915a3b2b2546fc05bc40
Simon Kwabla Azigie <simonziggy18@gmail.com>
2025-12-01 00:14:12 +0000

    new correction

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,8 +1,5 @@
-# bookshelf/forms.py
 from django import forms
-from .models import Book
 
-class BookForm(forms.ModelForm):
-    class Meta:
-        model = Book
-        fields = ['title', 'author', 'published_date']
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=100)
+    email = forms.EmailField()

a7c9d3cb334d4336025ab3b028162e489e071f63
Simon Kwabla Azigie <simonziggy18@gmail.com>
2025-12-07 23:53:45 +0000

    added auth views

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import UserChangeForm
+from .forms import UserRegisterForm
 
 # Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+class CustomLoginView(LoginView):
+    template_name = 'blog/login.html'
+
+class CustomLogoutView(LogoutView):
+    template_name = 'blog/logout.html'
+
+def profile(request):
+    if request.method == 'POST':
+        form = UserChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+    else:
+        form = UserChangeForm(instance=request.user)
+        
+    return render(request, 'blog/profile.html', {'form': form})
